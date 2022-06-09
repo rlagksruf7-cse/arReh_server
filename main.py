94e2b2d3c0e6a12472877f5a4c642ff31c46027b
Kim-Hankyeol <76488514+Kim-Hankyeol@users.noreply.github.com>
2022-06-10 04:56:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -174,7 +174,7 @@ if __name__ == "__main__":
     parser.add_argument("--cert-file", help="SSL certificate file (for HTTPS)")
     parser.add_argument("--key-file", help="SSL key file (for HTTPS)")
     parser.add_argument(
-        "--host", default="54.218.119.139", help="Host for HTTP server (default: 54.218.119.139)"
+        "--host", default="0.0.0.0", help="Host for HTTP server (default: 0.0.0.0)"
     )
     parser.add_argument(
         "--port", type=int, default=5000, help="Port for HTTP server (default: 5000)"
